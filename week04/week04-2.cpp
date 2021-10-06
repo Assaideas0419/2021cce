4e42a497e2836ddd5098cf29ed006e9d007a3818
Assaideas0419 <laiweisai.60105@gmail.com>
2021-10-06 12:03:17 +0800

    Add files via upload

---
+++ week04/week04-2.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int a=123,b=10;
+    int ans=a/b;
+    float ans2=123.0/10.0;
+    printf("%d\n",ans);
+    printf("%f\n",ans2);
+}
---
+++ week04/week04-3.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float pi=3.1415926535897932384626433832795;
+    double pii=3.1415926535897932384626433832795;
+    printf("%f\n",pi);
+    printf("%f\n",pii);
+    printf("%.10f\n",pii);
+    printf("%2.5f\n",pii);
+}
