ba2f218bb4469a344eb6b11febd9a3aaeb90dda0
Assaideas0419 <laiweisai.60105@gmail.com>
2021-10-13 11:57:19 +0800

    Add files via upload

---
+++ week05/week05-3.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    if (10>0) printf ("10>0成立\n");
+    if (10<0) printf ("10<0成立\n");
+    if (3) printf ("3成立\n");
+    if (2) printf ("2成立\n");
+    if (1) printf ("1成立\n");
+    if (0) printf ("0成立\n");
+    if (-1) printf ("-1成立\n");
+    if (-2) printf ("-2成立\n");
+    if (-3) printf ("-3成立\n");
+}
